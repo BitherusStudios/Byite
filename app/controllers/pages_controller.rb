e2a5d2e4347853b2cc71aa7e5e51e53899730d16
Bitherus <bitherusstudios@gmail.com>
2016-05-21 23:49:07 +0000

    Edited page titles for each page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,15 @@ class PagesController < ApplicationController
  
  #backend code
   def index
+    @title = "Home"
   end
-
   def home
+    @title = "Posts"
   end
-
   def profile
+   @title = "Profile"
   end
-
   def explore
+    @title = "Find" 
   end
 end
